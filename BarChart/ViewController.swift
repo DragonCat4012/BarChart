053c82912864c151d5b80daf3cd332ea415ec260
Kiara <frida.shivaa@gmail.com>
2021-12-11 14:45:56 +0100

    Initial Commit

---
+++ BarChart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarChart
+//
+//  Created by Akora on 11.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
